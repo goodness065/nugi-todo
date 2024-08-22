3e52d33989d3f00e6a3a78e6870597736d6965f8
goodness065 <goodnesskelechi2017@yahoo.com>
2024-08-22 23:53:02 +0100

    fix: fixed the issue of api not working on the vercel link

--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./app/router";
 
 async function enableMocking() {
-  if (process.env.NODE_ENV !== "development") {
-    return;
-  }
+  // if (process.env.NODE_ENV !== "development") {
+  //   return;
+  // }
 
   const { worker } = await import("./mocks/browser");
 
